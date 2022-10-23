923323d4cf2d747fc79be46860f70360580257f8
T.T <t.517a3@gmail.com>
2022-10-23 15:02:10 +0900

    初期化

--- plasearch-back/config/initializers/session_store.rb
+++
@@ -1,5 +0,0 @@
-if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_plasearch-back', domain: Rails.application.credentials.origin[:plasearch_front]
-else
-  Rails.application.config.session_store :cookie_store, key: '_plasearch-back'
-end
